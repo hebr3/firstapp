171a688c4552bb606487d496609a079ace9605ed
hebr3 <brook057@cougars.csusm.edu>
2017-10-26 10:55:56 -0700

    Modified hello.py with fullstackpython.com example

--- hello/hello.py
+++ hello/hello.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
-@app.route("/")
 
-def index():
-  return "Hello, World!"
+@app.route("/")
+def hello_world():
+  return 'Hello, World!'
 
 if __name__ == '__main__':
-  app.run(port=5000, debug=True)
+  app.run()
